1922ef72e0111189b5f1b8f2e4dae8c0bc34c26d
joost <joost@vunderink.net>
2011-05-14 16:12:17 +0200

    Initial empty Card structure.

---
+++ src/main/java/org/antvillage/cards/Card.java
@@ -0,0 +1,5 @@
+package org.antvillage.cards;
+
+public class Card {
+
+}
---
+++ src/test/java/org/antvillage/cards/CardTest.java
@@ -0,0 +1,23 @@
+package org.antvillage.cards;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CardTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCardConstructor() {
+		assertEquals(0, 0);
+	}
+}
