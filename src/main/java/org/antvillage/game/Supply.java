6fedb0a292ecded0a7619046e8456fa1c91b7eb5
Verik <Verik@localhost>
2011-05-14 17:39:15 +0200

    Implement initial structure and add Junit tests

---
+++ src/main/java/org/antvillage/game/Supply.java
@@ -0,0 +1,65 @@
+package org.antvillage.game;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.antvillage.cards.Cards;
+
+/**
+ * This class defines the supply that is used in a single game.
+ * 
+ * @author Verik
+ */
+public class Supply {
+
+	private static final int[] VICTORY_CARDS_FOR_PLAYERS = { 0, 8, 8, 12, 12 };
+
+	public Map<Cards, Integer> stacks = new HashMap<Cards, Integer>();
+	public int emptyStacks = 0;
+
+	public Supply() {
+		super();
+	}
+
+	public void takeCard(Cards card) {
+		Integer count = stacks.get(card);
+		if (count == null || count == 0) {
+			throw new RuntimeException("Card " + card + " not present in supply.");
+		}
+		
+		count --;
+		if ( count == 0) {
+			this.emptyStacks ++;
+		}
+
+		stacks.put(card, count );
+	}
+
+	public void init(int number_of_players, List<Cards> kingdomCards) {
+		createStack(Cards.COPPER, 60);
+		createStack(Cards.SILVER, 40);
+		createStack(Cards.GOLD, 30);
+
+		int numberOfCurses = (number_of_players - 1) * 10;
+		createStack(Cards.CURSE, numberOfCurses);
+
+		int numberOfVictoryCards = VICTORY_CARDS_FOR_PLAYERS[number_of_players];
+		createStack(Cards.ESTATE, numberOfVictoryCards);
+		createStack(Cards.DUCHY, numberOfVictoryCards);
+		createStack(Cards.PROVINCE, numberOfVictoryCards);
+
+		for (Cards kingdomCard : kingdomCards) {
+			int stackSize = 10;
+			if (kingdomCard.card.isVictory()) {
+				stackSize = numberOfVictoryCards;
+			}
+			createStack(kingdomCard, stackSize);
+		}
+	}
+
+	private void createStack(Cards card, int count) {
+		stacks.put(card, count);
+	}
+
+}
---
+++ src/test/java/org/antvillage/game/SupplyTest.java
@@ -0,0 +1,120 @@
+package org.antvillage.game;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.antvillage.cards.Cards;
+import org.antvillage.game.Supply;
+import org.junit.Test;
+
+public class SupplyTest {
+
+	@Test
+	public void testInitOnePlayer() {
+		Supply supply = new Supply();
+		
+		assertEquals(0, supply.emptyStacks);
+		assertEquals(0, supply.stacks.size());
+		
+		supply.init(1, new ArrayList<Cards>());
+		
+		assertEquals(0, supply.emptyStacks);
+		assertEquals(7, supply.stacks.size());
+		
+		assertTreasureCards(supply);
+		assertVictoryCards(supply, 8);
+		assertCurses(supply,0);
+	}
+
+	@Test
+	public void testInitTwoPlayers() {
+		Supply supply = new Supply();
+		supply.init(2, new ArrayList<Cards>());
+
+		assertTreasureCards(supply);
+		assertVictoryCards(supply, 8);
+		assertCurses(supply,10);
+	}
+
+	@Test
+	public void testInitThreePlayers() {
+		Supply supply = new Supply();
+		supply.init(3, new ArrayList<Cards>());
+
+		assertTreasureCards(supply);
+		assertVictoryCards(supply, 12);
+		assertCurses(supply,20);
+	}
+
+	@Test
+	public void testInitFourPlayers() {
+		Supply supply = new Supply();
+		supply.init(4, new ArrayList<Cards>());
+
+		assertTreasureCards(supply);
+		assertVictoryCards(supply, 12);
+		assertCurses(supply,30);
+	}
+	
+	@Test
+	public void takeCard() {
+		Supply supply = new Supply();
+		supply.init(2, new ArrayList<Cards>());
+		
+		try {
+			supply.takeCard(Cards.SMITHY);
+			fail("Failed to detect absense of smithy in supply");
+		}
+		catch (RuntimeException expected) {
+			// successfully detected absence
+		}
+		
+		takeCardAndAssert(supply, 9, 0);
+		takeCardAndAssert(supply, 8, 0);
+		takeCardAndAssert(supply, 7, 0);
+		takeCardAndAssert(supply, 6, 0);
+		takeCardAndAssert(supply, 5, 0);
+		takeCardAndAssert(supply, 4, 0);
+		takeCardAndAssert(supply, 3, 0);
+		takeCardAndAssert(supply, 2, 0);
+		takeCardAndAssert(supply, 1, 0);
+		takeCardAndAssert(supply, 0, 1);
+
+		try {
+			supply.takeCard(Cards.CURSE);
+			fail("Failed to detect zero left of GOLD in supply");
+		}
+		catch (RuntimeException expected) {
+			// successfully detected absence
+		}
+}
+
+	private void takeCardAndAssert(Supply supply, Integer expectedLeft, int stacksEmpty) {
+		supply.takeCard(Cards.CURSE);
+		assertEquals(expectedLeft, supply.stacks.get(Cards.CURSE));
+		assertEquals(stacksEmpty, supply.emptyStacks);
+	}
+
+	private void assertVictoryCards(Supply supply, Integer expectedVictoryCards) {
+		assertEquals(expectedVictoryCards, supply.stacks.get(Cards.ESTATE));
+		assertEquals(expectedVictoryCards, supply.stacks.get(Cards.DUCHY));
+		assertEquals(expectedVictoryCards, supply.stacks.get(Cards.PROVINCE));
+	}
+
+	private void assertCurses(Supply supply, Integer expectedCurses) {
+		assertEquals(expectedCurses, supply.stacks.get(Cards.CURSE));
+	}
+
+	private void assertTreasureCards(Supply supply) {
+		assertEquals((Integer)60, supply.stacks.get(Cards.COPPER));
+		assertEquals((Integer)40, supply.stacks.get(Cards.SILVER));
+		assertEquals((Integer)30, supply.stacks.get(Cards.GOLD));
+	}
+
+
+
+//	public void testTakeCard() {
+//	}	
+
+}
