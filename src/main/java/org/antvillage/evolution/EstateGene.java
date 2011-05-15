2326a38eda7b064508c3a97ce95434bbed90676e
joost <joost@vunderink.net>
2011-05-15 22:00:50 +0200

    Gave DuchyGene and EstateGene their own randomizeParameters() to give the value for provincesLeft more useful values (0-8 instead of 0-100).

--- src/main/java/org/antvillage/evolution/EstateGene.java
+++ src/main/java/org/antvillage/evolution/EstateGene.java
@@ -4,10 +4,17 @@ import org.antvillage.cards.Cards;
 import org.antvillage.game.Player;
 
 public class EstateGene extends Gene {
+	public final int PARAMETER_PROVINCES_LEFT_MAX = 8;
 	
 	public EstateGene() {
 		addParameter("value");
 		addParameter("provincesLeft");
+	}
+	
+	public void randomizeParameters() {
+		super.randomizeParameters();
+		float randomValue = (float)(PARAMETER_PROVINCES_LEFT_MAX * Math.random() + 1.0f);
+		setParameter("provincesLeft", randomValue);
 	}
 	
 	public void calculateBuyValues(CardValues currentValues, Player player) {
