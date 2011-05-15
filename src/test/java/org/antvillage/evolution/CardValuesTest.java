b6c4bdc6969708aed38be1d4b17e25006f0efad4
joost <joost@vunderink.net>
2011-05-15 19:26:47 +0200

    Added more evolution related tests. Added Gene.randomizeParameters().

--- src/test/java/org/antvillage/evolution/CardValuesTest.java
+++ src/test/java/org/antvillage/evolution/CardValuesTest.java
@@ -1,6 +1,7 @@
 package org.antvillage.evolution;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.util.LinkedList;
 
@@ -9,7 +10,6 @@ import org.antvillage.cards.Cards;
 import org.antvillage.game.Supply;
 import org.junit.Test;
 
-
 public class CardValuesTest {
 	@Test
 	public void testPutAndGet() {
@@ -17,7 +17,35 @@ public class CardValuesTest {
 		cardValues.put(Cards.COPPER, 1.0f);
 		assertEquals(cardValues.get(Cards.COPPER), 1.0f, 0.001f);
 	}
-	
+
+	@Test
+	public void testChangeValue() {
+		CardValues cardValues = new CardValues();
+		cardValues.put(Cards.COPPER, 0.0f);
+		cardValues.changeValue(Cards.COPPER, 1.0f);
+		assertEquals(cardValues.get(Cards.COPPER), 1.0f, 0.001f);
+	}
+
+	@Test
+	public void testChangeValueNonexistingCard() {
+		CardValues cardValues = new CardValues();
+		try {
+			cardValues.changeValue(Cards.COPPER, 1.0f);
+			fail("Failed to change value of nonexisting card in CardValues");
+		} catch (RuntimeException expected) {
+			// successfully detected absence
+		}
+	}
+
+	@Test
+	public void testClear() {
+		CardValues cardValues = new CardValues();
+		cardValues.put(Cards.COPPER, 1.0f);
+		assertEquals(cardValues.get(Cards.COPPER), 1.0f, 0.001f);
+		cardValues.clear();
+		assertEquals(cardValues.get(Cards.COPPER), 0.0f, 0.001f);
+	}
+
 	@Test
 	public void testGetDesiredCard() {
 		CardValues cardValues = new CardValues();
@@ -25,18 +53,28 @@ public class CardValuesTest {
 		cardValues.put(Cards.GOLD, 2.2f);
 		assertEquals(cardValues.getDesiredCard(), Cards.GOLD);
 	}
-	
+
+	@Test
+	public void testGetDesiredCardFail() {
+		CardValues cardValues = new CardValues();
+		try {
+			cardValues.getDesiredCard();
+			fail("Failed to get desired card for empty cardValues");
+		} catch (RuntimeException expected) {
+			// successfully detected absence
+		}
+	}
+
 	@Test
 	public void testInitFromSupply() {
 		CardValues cardValues = new CardValues();
 		Supply supply = new Supply();
 		supply.init(2, new LinkedList<Card>());
-		
+
 		cardValues.initFromSupply(supply);
 		assertEquals(cardValues.get(Cards.COPPER), 0.0f, 0.001f);
 		assertEquals(cardValues.get(Cards.ESTATE), 0.0f, 0.001f);
 		assertEquals(cardValues.get(Cards.CURSE), 0.0f, 0.001f);
 	}
-	
-	
+
 }
---
+++ src/test/java/org/antvillage/evolution/GeneTest.java
@@ -0,0 +1,97 @@
+package org.antvillage.evolution;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.antvillage.game.Player;
+import org.junit.Test;
+
+
+public class GeneTest {
+	@Test
+	public void testAddParameter() {
+		Gene gene = new Gene();
+		String param = "test";
+		gene.addParameter(param);
+		
+		assertEquals(gene.getParameter(param), 0.0f, 0.001f);
+	}
+	
+	@Test
+	public void testAddParameterTwiceFails() {
+		Gene gene = new Gene();
+		String param = "testparam";
+		gene.addParameter(param);
+		try {
+			gene.addParameter(param);
+			fail("Failed to add parameter " + param + " twice");
+		} catch (RuntimeException expected) {
+			// successfully detected absence
+		}
+
+	}
+
+	@Test
+	public void testSetGetParameter() {
+		Gene gene = new Gene();
+		String param = "test";
+		float value = 4.2f;
+		gene.addParameter(param);
+		gene.setParameter(param, value);
+		
+		assertEquals(gene.getParameter(param), value, 0.001f);
+	}
+
+	@Test
+	public void testGetNonExistingParameterFails() {
+		Gene gene = new Gene();
+		String param = "testparam";
+		try {
+			gene.getParameter(param);
+			fail("Failed to get non-existing parameter " + param);
+		} catch (RuntimeException expected) {
+			// successfully detected absence
+		}
+
+	}
+
+	@Test
+	public void testSetNonExistingParameterFails() {
+		Gene gene = new Gene();
+		String param = "testparam";
+		try {
+			gene.setParameter(param, 1.0f);
+			fail("Failed to set non-existing parameter " + param);
+		} catch (RuntimeException expected) {
+			// successfully detected absence
+		}
+
+	}
+
+	@Test
+	public void testRandomizeParameter() {
+		Gene gene = new Gene();
+		String param = "test";
+		gene.addParameter(param);
+		assertEquals(gene.getParameter(param), 0.0f, 0.001f);
+
+		gene.randomizeParameters();
+		assertTrue(gene.getParameter(param) > 0.5f);
+	}
+
+	@Test
+	public void testCalculateBuyValuesFails() {
+		Gene gene = new Gene();
+		CardValues cardValues = new CardValues();
+		Player player = new Player();
+		
+		try {
+			gene.calculateBuyValues(cardValues, player);
+			fail("Failed to call calculateBuyValues which needs to be implemented by child classes");
+		} catch (RuntimeException expected) {
+			// successfully detected absence
+		}
+	}
+
+}
