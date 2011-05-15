d004a387a6dbe5abf1e3e94b86d379dca6bae91c
Verik <Verik@localhost>
2011-05-15 16:40:08 +0200

    Implement run and TournamentRound, add junit tests and delegate test data creation to helper class

---
+++ src/test/java/org/antvillage/simulator/TournamentRoundTest.java
@@ -0,0 +1,28 @@
+package org.antvillage.simulator;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.antvillage.cards.Card;
+import org.antvillage.cards.Cards;
+import org.antvillage.game.GameSetupHelper;
+import org.antvillage.game.Player;
+import org.junit.Test;
+
+public class TournamentRoundTest {
+
+	@Test
+	public void testStart() {
+		List<Player> players = GameSetupHelper.createTwoPlayerBmPoc();
+		List<Card> kingdomCards = GameSetupHelper.createSimpleKingdom(Cards.VILLAGE);
+
+		TournamentRound round = new TournamentRound();
+		round.kingdomCards = kingdomCards;
+		round.participants = players;
+		round.run.games = 11;
+		
+		round.start();
+	}
+
+}
