add724946f8626dccc7fa02da4b3b7ecdec672f4
Verik <Verik@localhost>
2011-05-15 21:44:08 +0200

    Implement first working version of simulation that uses an evolver to evolve generations

--- src/main/java/org/antvillage/simulator/Evolver.java
+++ src/main/java/org/antvillage/simulator/Evolver.java
@@ -1,39 +1,160 @@
 package org.antvillage.simulator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import org.antvillage.evolution.Bot;
+import org.antvillage.evolution.Gene;
 import org.antvillage.game.Player;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class manages the evolution of genes.
- *
+ * 
  * @author Verik
  */
 public class Evolver {
+	private static final Logger logger = LoggerFactory.getLogger(Evolver.class);
 
 	private BotFactory botFactory = new BotFactory();
 
+	public Random random = new Random();
 	public List<Bot> generation;
-	
+	public int generationNumber = 0;
+	public Map<Integer, List<Bot>> generationHistory = new HashMap<Integer, List<Bot>>();
+
 	public void init() {
 		botFactory.init();
 	}
-	
+
 	public void spawnNewRandomGeneration(int amountToSpawn) {
-		generation = new LinkedList<Bot>();
+		if (amountToSpawn % 8 != 0) {
+			throw new RuntimeException("amountToSpawn must be a multiple of 8 due to evolving mechanics. See Evolver.createChildren() method.");
+			
+		}
 		
-		for( int i = 0; i < amountToSpawn; i++ ) {
+		generation = new LinkedList<Bot>();
+
+		for (int i = 0; i < amountToSpawn; i++) {
 			Bot bot = botFactory.createRandomGenesBot();
 			generation.add(bot);
 		}
+		nextGeneration();
 	}
-	
+
 	public List<Player> getGenerationAsPlayers() {
 		List<Player> players = new ArrayList<Player>(generation);
 		return players;
 	}
+
+	public void evolve(List<Player> sortedPlayers) {
+		List<Bot> survivors = getTopHalfAsBots(sortedPlayers);
+
+		this.generation = createChildren(survivors);
 	
+		nextGeneration();
+	}
+
+	private void nextGeneration() {
+		if (logger.isDebugEnabled()) {
+			
+			int described = 0;
+			
+			logger.debug("----\nGeneration {}\n---", generationNumber);
+			for (Bot bot: generation) {
+				bot.describe();
+				described ++;
+				if (described >= 2) {
+					break;
+				}
+			}
+		}
+		
+		if (generationNumber % 10 == 0) {
+			generationHistory.put(generationNumber, generation);
+		}
+		generationNumber++;
+	}
+
+	private List<Bot> createChildren(List<Bot> survivors) {
+
+		List<Bot> children = new ArrayList<Bot>();
+
+		Iterator<Bot> iterator = survivors.iterator();
+
+		while (iterator.hasNext()) {
+			Bot one = iterator.next();
+			Bot two = iterator.next();
+			Bot three = iterator.next();
+			Bot four = iterator.next();
+
+			createTwoChildren(one, two, children);
+			createTwoChildren(three, four, children);
+			createTwoChildren(one, three, children);
+			createTwoChildren(two, four, children);
+		}
+
+		return children;
+	}
+
+	private void createTwoChildren(Bot father, Bot mother, List<Bot> children) {
+		Bot child1 = createChild(father, mother);
+		children.add(child1);
+		
+		Bot child2 = createChild(father, mother);
+		children.add(child2);
+	}
+
+	private Bot createChild(Bot father, Bot mother) {
+		Bot child = botFactory.createBot();
+
+		Iterator<Gene> fatherGeneIterator = father.genes.iterator();
+		Iterator<Gene> motherGeneIterator = mother.genes.iterator();
+		for (Gene gene : child.genes) {
+			Gene fatherGene = fatherGeneIterator.next();
+			Gene motherGene = motherGeneIterator.next();
+			if ( random.nextBoolean()) {
+				updateGene(gene, fatherGene);
+			}
+			else {
+				updateGene(gene, motherGene);
+			}
+		}
+
+		return child;
+	}
+
+	private void updateGene(Gene childeGene, Gene parentGene) {
+		for (String name: parentGene.parameters.keySet()) {
+			float parentValue = parentGene.getParameter(name);
+			float deviation = (float)random.nextGaussian();
+			
+			float childeValue = parentValue + deviation;
+			
+			childeGene.setParameter(name, childeValue);
+		}	
+	}
+
+	private List<Bot> getTopHalfAsBots(List<Player> sortedPlayers) {
+		int survivorCount = sortedPlayers.size() / 2;
+		List<Bot> survivors = new ArrayList<Bot>(survivorCount);
+
+		for (Player player : sortedPlayers) {
+			Bot bot = (Bot) player;
+			survivors.add(bot);
+			survivorCount--;
+			if (survivorCount == 0) {
+				break;
+			}
+		}
+
+		return survivors;
+	}
+
 }
--- src/main/java/org/antvillage/simulator/Simulator.java
+++ src/main/java/org/antvillage/simulator/Simulator.java
@@ -1,9 +1,15 @@
 package org.antvillage.simulator;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.antvillage.cards.Card;
+import org.antvillage.game.Player;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class provides the facilities to run simulations. 
@@ -11,20 +17,60 @@ import org.antvillage.cards.Card;
  * @author Verik
  */
 public class Simulator {
+	private static final Logger logger = LoggerFactory.getLogger(Simulator.class);
+	
+	private Evolver evolver;
 	
 	public void runRandomGeneration() {
-		Evolver evolver = new Evolver();
+		evolver = new Evolver();
 		evolver.init();
-		evolver.spawnNewRandomGeneration(4);
+		evolver.spawnNewRandomGeneration(8);
 		
+		runAndEvolveGeneration();
+	}
+	
+	public void runAndEvolveGeneration() {
 		List<Card> kingdomCards = new LinkedList<Card>();
-		
 		TournamentRound round = new TournamentRound();
 		round.kingdomCards = kingdomCards;
 		round.participants = evolver.getGenerationAsPlayers();;
-//		round.run.games = 11;
 		
-		round.start();
+		Map<Player, Integer> results = round.start();
+		
+		List<Player> sortedPlayers = sortByWins(results);
+		evolver.evolve(sortedPlayers);
+
 	}
 
+	public List<Player> sortByWins(Map<Player, Integer> wins) {
+		
+		List<Player> sortedPlayers = new LinkedList<Player>(wins.keySet());
+		
+		PlayerComparator comparator = new PlayerComparator(wins);
+		Collections.sort(sortedPlayers, comparator);
+
+//		logger.info("\nSorted results:");
+//		for (Player player: sortedPlayers) {
+//			logger.info("{} ", player);
+//		}
+
+		return sortedPlayers;
+	}
+	
+	class PlayerComparator implements Comparator<Player> {
+
+		Map<Player, Integer> wins;
+		
+		private PlayerComparator(Map<Player, Integer> wins) {
+			this.wins = wins;
+		}
+
+		public int compare(Player player1, Player player2) {
+			
+			int wins1 = wins.get(player1);
+			int wins2 = wins.get(player2);
+			
+			return wins2 - wins1;
+		}
+	}
 }
